123705a93c80e5b0a6c156f961f73d8e365b6f1e
SebastianBerven <bashberv@gmail.com>
2019-04-22 19:09:04 -0700

    Cleaned other two files

--- file_cleaner.py
+++ file_cleaner.py
@@ -1,44 +1,83 @@
 import utils
 
 def main():
-    table, header = clean_poverty()
-    header[0] = header[0][3:]
+    clean_poverty()
+    clean_rent()
+    clean_crime()
+
+
+def clean_crime():
+    table = []
+    infile = open("crime_data.csv", "r")
+
+    lines = infile.readlines()
+    header = lines.pop(0).strip().split(",")
+    header.insert(1, "State_Name")
     table.insert(0, header)
-    utils.write_table("poverty_data_clean.csv", table)
-    for line in table:
-        utils.convert_to_float(line)
-    print(table[1])
+    
+    for line in lines:
+        newline = remove_quotes(line)
+        newline = newline.strip() #removes whitespace characters
+        values = newline.split(",") #splits on comma
+        values.insert(1, values[0][-2:])
+        values[0] = values[0][:-3]
+        #utils.convert_to_float(values)
+        table.append(values)
+
+
+    utils.write_table("crime_data_clean.csv", table)
+    infile.close()
+
+
+def clean_rent():
+    table = []
+    infile = open("rent_data.csv", "r")
+
+    lines = infile.readlines()
+    for line in lines:
+        newline = line.strip() #removes whitespace characters
+        values = newline.split(",") #splits on comma
+        #utils.convert_to_float(values)
+        table.append(values)
+
+    utils.write_table("rent_data_clean.csv", table)
+    infile.close()
 
 def clean_poverty():
     table = []
     infile = open("poverty_data.csv", "r")
 
-
     lines = infile.readlines()
     for line in lines:
-        count = 0
-        newline = ''
-        for char in line:
-            if count == 0:
-                if char == '"':
-                    count = 1
-                else:
-                    newline += char
-            elif count == 1:
-                if char == '"':
-                    count = 0
-                elif char == ',':
-                    pass
-                else:
-                    newline += char
+        newline = remove_quotes(line)
         newline = newline.strip() #removes whitespace characters
         values = newline.split(",") #splits on comma
         #utils.convert_to_float(values)
         table.append(values)
+
     header = table.pop(0)
-
-
+    header[0] = header[0][3:]
+    table.insert(0, header)
+    utils.write_table("poverty_data_clean.csv", table)
     infile.close()
-    return table, header
+
+
+def remove_quotes(line):
+    newline = ''
+    count = 0
+    for char in line:
+        if count == 0:
+            if char == '"':
+                count = 1
+            else:
+                newline += char
+        elif count == 1:
+            if char == '"':
+                count = 0
+            elif char == ',':
+                pass
+            else:
+                newline += char
+    return newline
 
 main()
