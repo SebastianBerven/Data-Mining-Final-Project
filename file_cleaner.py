876895a7be1a5fca5857c1de9db9a9be730ef8c9
SebastianBerven <bashberv@gmail.com>
2019-04-30 22:15:20 -0700

    Added Combined Data

--- file_cleaner.py
+++ file_cleaner.py
@@ -4,6 +4,34 @@ def main():
     clean_poverty()
     clean_rent()
     clean_crime()
+    combine_datasets()
+
+
+def combine_datasets():
+    poverty_table, _ = utils.read_table("poverty_data_clean.csv", True)
+    crime_table, _ = utils.read_table("crime_data_clean.csv", True)
+    rent_table, _ = utils.read_table("rent_data_no_dups.csv", True)
+    combined_table = []
+    combined_header = ["County", "State",\
+        "Pov_Num_All","Pov_Pct_All","Median_Income", \
+        "Crime_Rate_per_100000","Murder","Rape","Robbery","Aggravated_Assault","Burglary","Larceny","Vehicle_Theft","Arson",\
+        "Population","Mean_Rent","Median_Rent"]
+    for poverty_row in poverty_table:
+        for crime_row in crime_table:
+            for rent_row in rent_table:
+                if poverty_row[2] == crime_row[0] and crime_row[0] == rent_row[3]:
+                    new_add = [poverty_row[2], poverty_row[1],\
+                        poverty_row[3], poverty_row[4], poverty_row[9],\
+                        crime_row[2], crime_row[3], crime_row[4], crime_row[5], crime_row[6], crime_row[7], crime_row[8], crime_row[9], crime_row[10], \
+                        crime_row[11], rent_row[9], rent_row[10]]
+                    combined_table.append(new_add)
+    
+    no_dups = dict(((x[0],x[1]), x) for x in combined_table)
+    new_table = list(no_dups.values())
+
+    new_table.insert(0, combined_header)
+    utils.write_table("combined_data.csv", new_table)
+
 
 
 def clean_crime():
@@ -41,6 +69,9 @@ def clean_rent():
         table.append(values)
 
     utils.write_table("rent_data_clean.csv", table)
+    no_dups = dict(((x[1],x[3]), x) for x in table)
+    new_table = list(no_dups.values())
+    utils.write_table("rent_data_no_dups.csv", new_table)
     infile.close()
 
 def clean_poverty():
@@ -60,7 +91,6 @@ def clean_poverty():
     table.insert(0, header)
     utils.write_table("poverty_data_clean.csv", table)
     infile.close()
-
 
 def remove_quotes(line):
     newline = ''
